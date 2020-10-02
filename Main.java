045e23ed7fb5b334e0c987ecddb1f5b32ea33767
ellag7121 <ellag7121@wrdsb.ca>
2020-10-02 18:36:09 +0000

    finished a4q6

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ * asks user of integer, tells them if it is a prime # or not
+ * @author Graham Ellacott
  */
 public class Main {
 
@@ -9,7 +10,36 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    // make the scanner to get input
+    Scanner input = new Scanner(System.in);
+
+    //init variables
+    int number; //the number given by the user
+    int counter = 2; //the variable to increase every time the loop repeats (starts at 2 because prime numbers are divisible by 1)
+    int remainder; //the remainder of number / counter
+
+    //ask user for number
+    System.out.println("Which number would you like to check?");
+    number = input.nextInt();
+
+    while(counter < number){ //repeat as long as the counter is less than the number
+      //check if number is divisible
+      remainder = number % counter;
+
+      //check if remainder is not 0
+      if (remainder == 0){
+        //tell the user it is not a prime
+        System.out.println(number + " is not a prime number");
+        break;
+      }
+
+      //increase the counter by 1
+      counter = counter + 1;
+    }
     
+    //check if the counter reached the given number without being divisible by anything
+    if (counter == number){
+      System.out.println(number + " is a prime number");
+    }
   }
 }
